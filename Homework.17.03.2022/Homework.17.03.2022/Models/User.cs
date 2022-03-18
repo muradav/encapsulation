a02e49e90c124c09732b9133fdc757470760de19
muradav <muradav@code.edu.az>
2022-03-18 17:04:04 +0400

    feat:encapsulation taskinin ishlenilmesi

---
+++ Homework.17.03.2022/Homework.17.03.2022/Models/User.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework._17._03._2022.Models
+{
+    class User
+    {
+        private string _username;
+        private int _age;
+        private string _password;
+
+
+        public  string Username
+        { 
+            get; 
+            set; 
+        }
+        public int Age
+        {
+            get
+
+            {
+                return _age;
+            }
+
+            set
+            {
+                
+                if (value < 0) 
+                    
+                {
+                    
+                    Console.WriteLine("Age can't be negative!");
+                    
+                    return;
+                }
+                _age = value;
+
+
+            }
+
+
+        }
+        public string Password 
+        {
+            get
+            {
+                return _password;
+            }
+            set 
+            {
+                
+                if (string.IsNullOrEmpty(value)== true || !(value.Length >= 8) || IsPasswordValid(value)== false)
+                {
+                    
+                    Console.WriteLine("Password invalid \n" +
+                    "Please use at least one upper case and one digit");
+                    Console.WriteLine("==========================");
+
+                    return;
+                }
+                _password = value;
+            } 
+        }
+
+        
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine($"Username: {Username} \n" +
+                $"Password: {Password} \n" +
+                $"Age: {Age}");
+        }
+
+
+        
+        public bool IsPasswordValid(string password)
+        {
+            char[] arr = password.ToCharArray();
+            
+            bool result = false;
+            bool result1 = false;
+            bool result2 = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (result1 != char.IsUpper(arr[i])) 
+                {
+                    result1 = true;
+                }
+
+
+                if (result2 != char.IsDigit(arr[i]))
+                {
+                    result2 = true;
+                }
+                if (result1 == true && result2 == true) 
+                {
+                    result = true;
+
+                }
+                
+
+
+
+            }
+
+            return result;
+            
+            
+          
+                   
+        }
+    }
+}
---
+++ Homework.17.03.2022/Homework.17.03.2022/Program.cs
@@ -0,0 +1,47 @@
+﻿using Homework._17._03._2022.Models;
+using System;
+
+namespace Homework._17._03._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             User class
+            - Username
+            - Age
+            - Password
+            Username, password property-ləri olmadan user obyekti yaratmaq olmaz.
+            Age propery-i mənfi ola bilməz.
+            Password property-i bu şərtləri ödəməlidir:
+            1. Boş ola bilməz
+            2. Uzunluğu 8-dən kiçik ola bilməz
+            3. İçində ən az bir böyük hərf olmalıdır
+            4. İçində ən az bir rəqəm olmalıdır
+            Üçüncü və dördüncü şərti ayrı bir private methodda yazıb gəlib propertyinin setində istifadə etmək lazımdır.
+             */
+
+           
+            Console.Write("Please enter the username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Please enter the password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Please enter the age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("==========================");
+
+            
+            User user1 = new User(username,password);
+            user1.Age = age;
+            user1.GetInfo();
+
+            
+            
+            
+        }
+    }
+}
